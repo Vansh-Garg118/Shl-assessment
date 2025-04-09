14ec2c233b745fc7cedeb4732b89358c55bc23fc
Vansh Garg <vanshgarg25804@gmail.com>
2025-04-09 15:01:14 +0530

    fix bugs

--- vite.config.js
+++ vite.config.js
@@ -3,14 +3,14 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
-  server: {
-    // proxy: {
-    //   "/api": {
-    //   target: "https://shl-assessment-backend-vanshgarg.up.railway.app/api",
-    //   changeOrigin: true,
-    //   rewrite: (path) => path.replace(/^\/api/, ""),
-    // },
-    // }  
-  } ,
+  // server: {
+  //   // proxy: {
+  //   //   "/api": {
+  //   //   target: "https://shl-assessment-backend-vanshgarg.up.railway.app/api",
+  //   //   changeOrigin: true,
+  //   //   rewrite: (path) => path.replace(/^\/api/, ""),
+  //   // },
+  //   // }  
+  // } ,
   plugins: [tailwindcss(),react()],
 })
